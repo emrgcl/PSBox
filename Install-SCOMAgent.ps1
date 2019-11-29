99a3c2fd629e7d79711d0bde2f56b5b37f1540d0
Emre Guclu <41307814+emrgcl@users.noreply.github.com>
2019-11-29 12:05:17 +0300

    updated

--- Install-SCOMAgent.ps1
+++ Install-SCOMAgent.ps1
@@ -1,32 +1,71 @@
 <#
 .Synopsis
-   Installs scom agent Silenlty for the specified management server
+   Installs Scom Agent using hardcoded dynamic settings
 .DESCRIPTION
-   Installs scom agent Silenlty for the specified management server
-.EXAMPLE
-   .\Install-ScomAgent.ps1 -SourcePath 'C:\Source\AMD64' -MGName 'Your_ManagementGroup_Name' -PrimaryMS 'ms1.contoso.com'
-   VERBOSE: [3/11/2019 1:49:09 PM] Running on server SRVDMZ1MOBILE2
-   VERBOSE: [3/11/2019 1:49:09 PM] CommandLine is msiexec.exe /i C:\Source\AMD64\MOMAgent.msi /qn USE_SETTINGS_FROM_AD=0 MANAGEMENT_GROUP=Your_ManagementGroup_Name MANAGEMENT_SERVER_DNS=ms1.contoso.com MANAGEMENT_SERVER_AD_NAME=ms1.contoso.com ACTIONS_USE_COMPUTER_ACCOUNT=1 USE_MANUALLY_SPECIFIED_SETTINGS=1 AcceptEndUserLicenseAgreement=1
+   Installs Scom Agent using hardcoded dynamic settings
+.EXAMPLE 
+    .\Install-ScomAgent.ps1 -MGName 'SCOMMNG' -Verbose
+    
+    VERBOSE: Cannot install agent on SRVDMZ1SCOMGW. IT can be a managementserver or an agent is already installed.
+    
+   
+.EXAMPLE 
+    .\Install-ScomAgent.ps1 -MGName 'SCOMMNG' -WhatIf -Verbose
+
+    What if: Performing the operation "msiexec.exe /i zzzz\MOMAgent.msi /qn USE_SETTINGS_FROM_AD=0 MANAGEMENT_GROUP=SCOMMNG MANAGEMENT_SERVER_DNS=srvdmz1scomgw.DMZDOMAIN.local MANAGEMENT_SERVER_AD_NAME=srvdmz1scomgw.DMZDOMAIN.local ACTIONS_US
+E_COMPUTER_ACCOUNT=1 USE_MANUALLY_SPECIFIED_SETTINGS=1 AcceptEndUserLicenseAgreement=1" on target "SRVDMZ1SCOMGW".
+    
+
+    Script supports simiulating the operation. 
 #>
+[CmdletBinding(SupportsShouldProcess=$true)]
+Param(
 
-    [CmdletBinding()]
-    Param
-    (
-        # Source path to momagent.msi
-        [Parameter(Mandatory=$true)]
-        [string]$SourcePath='.',
+[Parameter(Mandatory = $true)]
+[string]$MGName
 
-        [Parameter(Mandatory=$true)]
-        [string]$PrimaryMS='SRVDMZ1SCOM.emlakdmz.local',
+)
 
-        [Parameter(Mandatory=$true)]
-        [string]$MGName='SCOM_EMLAK_GROUP'
+$ManagementServerRegistry = @{
 
-    )
+'LOCALDOMAIN' = @(@{PrimaryMS = 'srvscomms1.LOCALDOMAIN.local';SourcePath = '\\fileserver\d$\Sccm_sources\Software\SCOM2019\Agent\AMD64'},@{PrimaryMS='srvscomms2.LOCALDOMAIN.local';SourcePath='\\fileserver\d$\Sccm_sources\Software\SCOM2019\Agent\AMD64'})
+
+'DMZDOMAIN' = @{ 
+
+            '172.20.1' =@{PrimaryMS='srvdmz1scomgw.DMZDOMAIN.local';SourcePath='zzzz'}
+            '172.20.2' =@{PrimaryMS='srvdmz2scomgw.DMZDOMAIN.local';SourcePath='ttttt'}
+
+           }
 
 
+}
 
-$CommandLine="msiexec.exe /i $SourcePath\MOMAgent.msi /qn USE_SETTINGS_FROM_AD=0 MANAGEMENT_GROUP=$MGName MANAGEMENT_SERVER_DNS=$PrimaryMS MANAGEMENT_SERVER_AD_NAME=$PrimaryMS ACTIONS_USE_COMPUTER_ACCOUNT=1 USE_MANUALLY_SPECIFIED_SETTINGS=1 AcceptEndUserLicenseAgreement=1"
+$CanInstallAgent='srvdmz1scomgw','srvdmz2scomgw','srvscomms1','srvscomms2' -notcontains $env:COMPUTERNAME
+
+
+$Settings = switch ($env:USERDOMAIN) {
+
+
+'LOCALDOMAIN' {$ManagementServerRegistry.LOCALDOMAIN | Get-Random}
+'DMZDOMAIN' {$ManagementServerRegistry.DMZDOMAIN."$((Get-NetIPAddress).IPAddress | ForEach-Object  {if($_ -match '(?<DMZ>172\.20\.(1|2))') {$Matches['DMZ']}})"}
+
+
+}
+
+$ScomAgent = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -eq 'Microsoft Monitoring Agent'}| Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+
+if(!$ScomAgent -and $CanInstallAgent) {
+
+$CommandLine="msiexec.exe /i $($Settings.SourcePath)\MOMAgent.msi /qn USE_SETTINGS_FROM_AD=0 MANAGEMENT_GROUP=$MGName MANAGEMENT_SERVER_DNS=$($Settings.PrimaryMS) MANAGEMENT_SERVER_AD_NAME=$($Settings.PrimaryMS) ACTIONS_USE_COMPUTER_ACCOUNT=1 USE_MANUALLY_SPECIFIED_SETTINGS=1 AcceptEndUserLicenseAgreement=1"
 Write-Verbose "[$(Get-Date -Format G)] Running on server $($env:COMPUTERNAME)"
 Write-Verbose "[$(Get-Date -Format G)] CommandLine is $CommandLine"
+if ($pscmdlet.ShouldProcess($Env:ComputerName, $CommandLine))
+{
+        
 Invoke-Expression $commandline -Verbose:$VerbosePreference 
+}
+
+} else {
+
+Write-Verbose -Message "Cannot install agent on $($Env:ComputerName). IT can be a managementserver or an agent is already installed."
+} 
